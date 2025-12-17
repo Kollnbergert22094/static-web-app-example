a98532ebdc314a6267f93faf3f838113c23d8697
Kollnbergert22094 <Kollnbergert22094@sus.htl-grieskirchen.at>
2025-12-17 18:45:33 +0100

    Use Key Vault Connection string

--- WebApplication/MyWebApplication.Db/WeatherDatabaseContext.cs
+++ WebApplication/MyWebApplication.Db/WeatherDatabaseContext.cs
@@ -17,7 +17,5 @@ public class WeatherDatabaseContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-
-        optionsBuilder.UseSqlServer("Server=tcp:sql-server-kollnbergert22094.database.windows.net,1433;Initial Catalog=sql-database-kollnbergert22094;Persist Security Info=False;User ID=kollnbergt22094;Password=htlgkr1#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
 }
